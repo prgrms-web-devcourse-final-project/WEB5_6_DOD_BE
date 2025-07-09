68dae34234e9ab149436c56a68c61af844a7c4e0
Leeka99 <dsrn0000@gmail.com>
2025-07-09 14:06:14 +0900

    ✨ feat : repository 추가

--- src/main/java/com/grepp/spring/app/model/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/grepp/spring/app/model/schedule/repository/ScheduleRepository.java
@@ -1,6 +1,8 @@
 package com.grepp.spring.app.model.schedule.repository;
 
+import com.grepp.spring.app.model.schedule.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class ScheduleRepository extends JpaRepository<> {
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
 }
