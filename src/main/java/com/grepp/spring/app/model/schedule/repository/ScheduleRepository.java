dbd83447109fe691129b5a65d5b1a99e3d07ab58
Leeka99 <dsrn0000@gmail.com>
2025-07-10 15:31:09 +0900

    ✨ feat: 일정 조회

--- src/main/java/com/grepp/spring/app/model/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/grepp/spring/app/model/schedule/repository/ScheduleRepository.java
@@ -1,7 +1,10 @@
 package com.grepp.spring.app.model.schedule.repository;
 
 import com.grepp.spring.app.model.schedule.entity.Schedule;
+import io.lettuce.core.dynamic.annotation.Param;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 }
