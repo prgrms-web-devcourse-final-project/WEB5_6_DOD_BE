d682166253a93cfeedf8944f8fc375d818fce4f9
Leeka99 <dsrn0000@gmail.com>
2025-07-11 15:04:07 +0900

    ✨ feat : deleteScheduleService

--- src/main/java/com/grepp/spring/app/model/schedule/repository/ScheduleMemberRepository.java
+++ src/main/java/com/grepp/spring/app/model/schedule/repository/ScheduleMemberRepository.java
@@ -3,9 +3,10 @@ package com.grepp.spring.app.model.schedule.repository;
 import com.grepp.spring.app.model.schedule.entity.ScheduleMember;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ScheduleMemberRepository extends JpaRepository<ScheduleMember, Long> {
 
     List<ScheduleMember> findByScheduleId(Long scheduleId);
-
 }
