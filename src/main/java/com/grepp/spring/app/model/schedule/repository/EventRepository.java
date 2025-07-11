19a3c99fc661fd780918d0a205364e24fcaa7f7a
Leeka99 <dsrn0000@gmail.com>
2025-07-12 00:47:07 +0900

    ♻️ refactor: deleteEventRepository from schedule

--- src/main/java/com/grepp/spring/app/model/schedule/repository/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.grepp.spring.app.model.schedule.repository;
-
-import com.grepp.spring.app.model.event.entity.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// 나중에 Event 패키지의 레포로 변경 후 삭제
-public interface EventRepository extends JpaRepository<Event, Long> {
-
-}
