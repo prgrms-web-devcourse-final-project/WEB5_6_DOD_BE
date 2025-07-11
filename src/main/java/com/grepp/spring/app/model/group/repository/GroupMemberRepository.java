f4c3fcafb222991e461e3aa30c3a37e67df420b0
Seojun Lee <leeseojun34@gmail.com>
2025-07-11 14:29:06 +0900

    🐛 hotfix: Group 관련 Repository 중복 오류 수정

--- src/main/java/com/grepp/spring/app/model/group/repository/GroupMemberRepository.java
+++ src/main/java/com/grepp/spring/app/model/group/repository/GroupMemberRepository.java
@@ -2,9 +2,12 @@ package com.grepp.spring.app.model.group.repository;
 
 import com.grepp.spring.app.model.group.entity.GroupMember;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> {
 
     List<GroupMember> findByMemberId(String memberId);
+    Optional<GroupMember> findByGroupIdAndMemberId(Long groupId, String memberId);
 }
