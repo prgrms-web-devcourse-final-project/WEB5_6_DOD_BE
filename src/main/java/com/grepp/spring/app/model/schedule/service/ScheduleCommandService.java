3937ca44ce2126cbef476d6db1a73410662dd56f
Leeka99 <dsrn0000@gmail.com>
2025-07-12 20:21:18 +0900

    🐛 fix: Service, Repository CQRS 로직으로 분리

--- src/main/java/com/grepp/spring/app/model/schedule/service/ScheduleCommandService.java
+++ src/main/java/com/grepp/spring/app/model/schedule/service/ScheduleCommandService.java
@@ -1,6 +1,7 @@
 package com.grepp.spring.app.model.schedule.service;
 
 import com.grepp.spring.app.controller.api.schedules.payload.request.CreateSchedulesRequest;
+import com.grepp.spring.app.controller.api.schedules.payload.response.ShowScheduleResponse;
 import com.grepp.spring.app.model.event.entity.Event;
 import com.grepp.spring.app.model.event.repository.EventRepository;
 import com.grepp.spring.app.model.member.entity.Member;
@@ -8,13 +9,16 @@ import com.grepp.spring.app.model.member.repository.MemberRepository;
 import com.grepp.spring.app.model.schedule.code.ScheduleRole;
 import com.grepp.spring.app.model.schedule.dto.CreateScheduleDto;
 import com.grepp.spring.app.model.schedule.dto.ScheduleMemberRolesDto;
+import com.grepp.spring.app.model.schedule.dto.ShowScheduleDto;
 import com.grepp.spring.app.model.schedule.entity.Schedule;
 import com.grepp.spring.app.model.schedule.entity.ScheduleMember;
+import com.grepp.spring.app.model.schedule.entity.Workspace;
 import com.grepp.spring.app.model.schedule.repository.ScheduleCommandRepository;
 import com.grepp.spring.app.model.schedule.repository.ScheduleMemberQueryRepository;
+import com.grepp.spring.app.model.schedule.repository.ScheduleQueryRepository;
 import com.grepp.spring.app.model.schedule.repository.WorkspaceQueryRepository;
 import com.grepp.spring.infra.error.exceptions.NotFoundException;
-import java.util.Map;
+import java.util.List;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +30,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class ScheduleCommandService {
 
     @Autowired private ScheduleCommandRepository scheduleCommandRepository;
+    @Autowired private ScheduleQueryRepository scheduleQueryRepository;
     @Autowired private ScheduleMemberQueryRepository scheduleMemberQueryRepository;
     @Autowired private WorkspaceQueryRepository workspaceQueryRepository;
 
@@ -36,13 +41,13 @@ public class ScheduleCommandService {
 
 //    @Transactional
     public ShowScheduleResponse showSchedule(Long scheduleId) {
-        Optional<Schedule> schedule = scheduleRepository.findById(scheduleId);
+        Optional<Schedule> schedule = scheduleQueryRepository.findById(scheduleId);
 
         // Lazy init 해결하기 위해서 Transactional 내에서 처리
         Long eventId = schedule.get().getEvent().getId();
 
-        List<ScheduleMember> scheduleMembers = scheduleMemberRepository.findByScheduleId(scheduleId);
-        List<Workspace> workspaces = workspaceRepository.findAllByScheduleId(scheduleId);
+        List<ScheduleMember> scheduleMembers = scheduleMemberQueryRepository.findByScheduleId(scheduleId);
+        List<Workspace> workspaces = workspaceQueryRepository.findAllByScheduleId(scheduleId);
 
         ShowScheduleDto dto = ShowScheduleDto.fromEntity(eventId, schedule.orElse(null), scheduleMembers, workspaces);
 
