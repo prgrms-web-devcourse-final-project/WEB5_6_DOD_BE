ae6ec6cbf38f8cd5e960b96bdd7619e7360c087c
Seojun Lee <leeseojun34@gmail.com>
2025-07-14 22:32:16 +0900

    ♻️ refactor: 불필요한 ENUM 삭제

--- src/main/java/com/grepp/spring/app/model/schedule/code/ScheduleStatus.java
+++ src/main/java/com/grepp/spring/app/model/schedule/code/ScheduleStatus.java
@@ -1,5 +1,5 @@
 package com.grepp.spring.app.model.schedule.code;
 
 public enum ScheduleStatus {
-    L_RECOMMEND, E_RECOMMEND, FIXED, COMPLETE, NONE
+    L_RECOMMEND, E_RECOMMEND, FIXED, COMPLETE
 }
