598272b80c3bc48c00df7bc22a6f60b7506463c4
june3780 <112941412+june3780@users.noreply.github.com>
2025-07-15 10:31:39 +0900

    ✨ feat	그룹 삭제 - 예외 처리
    ✨ feat	그룹 삭제 시 발생하는 예외에 대한 처리

--- src/main/java/com/grepp/spring/app/model/group/repository/GroupMemberCommandRepository.java
+++ src/main/java/com/grepp/spring/app/model/group/repository/GroupMemberCommandRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GroupMemberCommandRepository extends JpaRepository<GroupMember, Long> {
 
+    void deleteByGroupId(Long groupId);
 }
