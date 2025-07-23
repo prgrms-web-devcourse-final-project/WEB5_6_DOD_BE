682adc2ad07007b1bcd5bb3a9d8ebf88a33625cd
june3780 <112941412+june3780@users.noreply.github.com>
2025-07-23 21:38:50 +0900

    ♻️ refactor	groupMember의 기본 생성자 추가
    ♻️ refactor	다른 도메인에서 GroupMember 객체 생성 시 필요한 생성자 추가

--- src/main/java/com/grepp/spring/app/model/group/entity/GroupMember.java
+++ src/main/java/com/grepp/spring/app/model/group/entity/GroupMember.java
@@ -18,6 +18,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
@@ -25,6 +26,7 @@ import lombok.Setter;
 @Table(name = "GroupMembers")
 @Getter
 @Setter
+@NoArgsConstructor
 public class GroupMember extends BaseEntity {
 
     @Id
