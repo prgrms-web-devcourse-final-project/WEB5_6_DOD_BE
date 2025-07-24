1c7d0e1dd6e025abdf1e7b2a175fd89f7a21ef3b
june3780 <112941412+june3780@users.noreply.github.com>
2025-07-25 03:03:24 +0900

    ✅ test	GroupCommandService에 대한 test코드 작성
    event와 Schedule에 의존하지 않는 비즈니스로직들에 대한 테스트코드 작성

--- src/main/java/com/grepp/spring/app/controller/api/group/payload/request/CreateGroupRequest.java
+++ src/main/java/com/grepp/spring/app/controller/api/group/payload/request/CreateGroupRequest.java
@@ -1,9 +1,11 @@
 package com.grepp.spring.app.controller.api.group.payload.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class CreateGroupRequest {
     // TODO: groupName 중복 가능한지: 프론트와 협의
     // TODO: groupName, description 의 문자열 개수 제한: 프론트와 협의
