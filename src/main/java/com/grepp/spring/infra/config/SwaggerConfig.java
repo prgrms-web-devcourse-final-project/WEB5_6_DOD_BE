321d9af3f22960e526160356961883c163dde7ea
qkqehenr7 <qkqehenr37@gmail.com>
2025-07-24 18:38:31 +0900

    📝 docs: CurrentUser 어노테이션 사용 시 Swagger에서 파라미터 요구 문제 해결

--- src/main/java/com/grepp/spring/infra/config/SwaggerConfig.java
+++ src/main/java/com/grepp/spring/infra/config/SwaggerConfig.java
@@ -1,21 +1,45 @@
 package com.grepp.spring.infra.config;
 
+import com.grepp.spring.infra.auth.CurrentUser;
+import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.media.Content;
 import io.swagger.v3.oas.models.media.MediaType;
 import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.HeaderParameter;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerMethod;
 
 
 @Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OperationCustomizer customizeOperation() {
+        return (Operation operation, HandlerMethod handlerMethod) -> {
+            // 해당 핸들러 메서드의 파라미터들을 순회
+            for (java.lang.reflect.Parameter methodParameter : handlerMethod.getMethod().getParameters()) {
+                // @CurrentUser 어노테이션이 붙어있는지 확인
+                if (methodParameter.isAnnotationPresent(CurrentUser.class)) {
+                    // 해당 파라미터를 Swagger 명세에서 제거
+                    // operation.getParameters()는 null일 수 있으므로 null 체크
+                    if (operation.getParameters() != null) {
+                        operation.getParameters().removeIf(p -> p.getName().equals(methodParameter.getName()));
+                    }
+                    // 보안 요구사항 추가 (OpenAPI 3.0의 SecurityScheme 설정이 선행되어야 함)
+                    operation.addSecurityItem(new SecurityRequirement().addList("bearerAuth")); // "bearerAuth"는 SecurityScheme에 정의된 이름
+                }
+            }
+            return operation;
+        };
+    }
 
     @Bean
     public OpenAPI openApiSpec() {
