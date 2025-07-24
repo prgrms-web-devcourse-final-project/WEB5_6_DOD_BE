7a2ef9bc1b6eba2e2a0bf82ed1e52b883e8bdb0e
june3780 <112941412+june3780@users.noreply.github.com>
2025-07-25 03:03:24 +0900

    ✅ test	GroupCommandService에 대한 test코드 작성
    event와 Schedule에 의존하지 않는 비즈니스로직들에 대한 테스트코드 작성

--- src/main/java/com/grepp/spring/app/controller/api/group/payload/request/ModifyGroupInfoRequest.java
+++ src/main/java/com/grepp/spring/app/controller/api/group/payload/request/ModifyGroupInfoRequest.java
@@ -1,8 +1,10 @@
 package com.grepp.spring.app.controller.api.group.payload.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ModifyGroupInfoRequest {
     private String groupName;
     private String description;
