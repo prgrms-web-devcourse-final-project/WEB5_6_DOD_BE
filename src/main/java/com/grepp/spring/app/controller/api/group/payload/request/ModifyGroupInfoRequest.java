f9d61774f0328f190c79abe06d8ea463ff416673
june3780 <112941412+june3780@users.noreply.github.com>
2025-07-28 08:51:36 +0900

    🐛 fix	불필요한 Data 대신 Getter로 변경
    🐛 fix	ModifyGroupInfoRequest에 과도하게 쓰인 Data 어노테이션 대신 Getter로 변경

--- src/main/java/com/grepp/spring/app/controller/api/group/payload/request/ModifyGroupInfoRequest.java
+++ src/main/java/com/grepp/spring/app/controller/api/group/payload/request/ModifyGroupInfoRequest.java
@@ -2,8 +2,9 @@ package com.grepp.spring.app.controller.api.group.payload.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 @AllArgsConstructor
 public class ModifyGroupInfoRequest {
     private String groupName;
