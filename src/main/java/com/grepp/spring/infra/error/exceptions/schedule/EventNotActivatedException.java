5618a935f89d575c9a91a3d51018689747d78847
Seojun Lee <leeseojun34@gmail.com>
2025-09-06 00:58:31 +0900

    ♻️ refactor: schedule 도메인 예외 처리 리팩터링
    전역 예외 처리에 맞게 schedule 도메인의 예외 처리 로직을 리팩터링
    
    주요 변경 사항:
    - ScheduleErrorCode: ErrorCode 인터페이스를 구현하도록 수정
    - schedule 예외 클래스: 모든 커스텀 예외가 CustomException을 상속하도록 변경
    - ScheduleExceptionAdvice: ApiCustomExceptionAdvice로 역할이 통합됨에 따라 삭제

--- src/main/java/com/grepp/spring/infra/error/exceptions/schedule/EventNotActivatedException.java
+++ src/main/java/com/grepp/spring/infra/error/exceptions/schedule/EventNotActivatedException.java
@@ -1,24 +1,18 @@
 package com.grepp.spring.infra.error.exceptions.schedule;
 
+import com.grepp.spring.infra.error.exceptions.CustomException;
 import com.grepp.spring.infra.response.ScheduleErrorCode;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class EventNotActivatedException extends RuntimeException {
-
-    private final ScheduleErrorCode code;
+public class EventNotActivatedException extends CustomException {
 
     public EventNotActivatedException(ScheduleErrorCode code) {
-        this.code = code;
+        super(code);
     }
 
     public EventNotActivatedException(ScheduleErrorCode code, Exception e) {
-        this.code = code;
-        log.error(e.getMessage(), e);
-    }
-
-    public ScheduleErrorCode code() {
-        return code;
+        super(code, e);
     }
 
 }
--- src/main/java/com/grepp/spring/infra/error/exceptions/schedule/WorkSpaceNotFoundException.java
+++ src/main/java/com/grepp/spring/infra/error/exceptions/schedule/WorkSpaceNotFoundException.java
@@ -1,22 +1,18 @@
 package com.grepp.spring.infra.error.exceptions.schedule;
 
+import com.grepp.spring.infra.error.exceptions.CustomException;
 import com.grepp.spring.infra.response.ScheduleErrorCode;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class WorkSpaceNotFoundException extends RuntimeException {
-    private final ScheduleErrorCode code;
+public class WorkSpaceNotFoundException extends CustomException {
 
     public WorkSpaceNotFoundException(ScheduleErrorCode code) {
-        this.code = code;
+        super(code);
     }
 
     public WorkSpaceNotFoundException(ScheduleErrorCode code, Exception e) {
-        this.code = code;
-        log.error(e.getMessage(), e);
+        super(code, e);
     }
 
-    public ScheduleErrorCode code() {
-        return code;
-    }
 }
